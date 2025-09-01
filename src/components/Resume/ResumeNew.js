e47a8108e57ab07af6ef72410eb37ca2da24803a
Rahmat Ashari <rahmatezdev@gmail.com>
2025-09-01 22:57:59 +0700

    Update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/RahmatAshariCV.pdf";
+import pdf from "../../Assets/../Assets/CV_Rahmat Ashari_Frontend&Backend.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 
